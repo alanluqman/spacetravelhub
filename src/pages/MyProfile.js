c223fab3ab79e482a5f3fff6f7afc9f93ec7c4ac
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-08-24 20:31:12 +0100

    Update myProfile.js file and Display.css file

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,6 +1,10 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
 import '../css/Display.css';
 
 export default function Display() {
+  const rockets = useSelector((state) => state.rockets);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
   return (
     <div className="container">
       <div className="display-section">
@@ -22,21 +26,11 @@ export default function Display() {
           </tbody>
         </table>
       </div>
-      <div className="display-section">
-        <h1>My Rockets</h1>
-        <table>
-          <tbody>
-            <tr>
-              <td>Rocket 1</td>
-            </tr>
-            <tr>
-              <td>Rocket 2</td>
-            </tr>
-            <tr>
-              <td>Rocket 3</td>
-            </tr>
-          </tbody>
-        </table>
+      <div className="rocket-container">
+        <h2>My Rockets</h2>
+        <ul className="rocket-list">
+          {reservedRockets.map((element) => <li key={element.id}>{element.name}</li>)}
+        </ul>
       </div>
     </div>
   );
